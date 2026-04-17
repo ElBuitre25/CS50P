2cd0b2248e24c70da5938f64d64cb96debaa3335
mathias velez <mathias25fcm@gmail.com>
2026-04-16 21:18:31 -0500

    mitad de set 1

---
+++ set1/deep.py
@@ -0,0 +1,16 @@
+def main():
+    great_question=input("¿what is the answer of the Great Question of Life? ").lower()
+    print(ans_great_qs(great_question))
+
+
+def ans_great_qs(great_question):
+    match great_question:
+        case "42" | "forty-two" |"forty two":
+            return "yes"
+        case _:
+            return "no"
+
+
+
+
+main() 
